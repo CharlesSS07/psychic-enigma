5ef372017894c6b7f0cbc9e9002d62dd2aa11823
charlesstrauss <u1319464@umail.utah.edu>
2023-12-31 14:47:21 -0700

    fixed up run.sh for nodejs

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-node node-chat-server/server.js
+
+ROOT_DIR=$(pwd)
+
+cd node-chat-server
+npm install >> $ROOT_DIR/logs/node-chat-server.log 2>&1
+npm start >> $ROOT_DIR/logs/node-chat-server.log 2>&1 &
+cd ..
