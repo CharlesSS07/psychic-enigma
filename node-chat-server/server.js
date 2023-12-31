ae151d9e148c56d246bfd82028656eece1ad1fa0
RustyMac <robertrstrauss@icloud.com>
2023-12-31 14:28:30 -0700

    added working nodejs chatserver core

---
+++ node-chat-server/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require("path");
+// var express = requrire('express');
+
+__dirname = path.resolve(path.dirname(''));
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/style.css');
+    // res.sendFile(__dirname + '/folder/');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('leave', function(otherName) {
+        io.emit('leave', otherName);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
